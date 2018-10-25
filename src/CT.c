b2b880675272c1bc937e657031141cc9d935c936
miaoyu9 <41392510+miaoyu9@users.noreply.github.com>
2018-10-25 09:01:07 -0400

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -224,8 +224,16 @@ void CT(int n, double *y[], double *x, int nclass, int edge, double *improve, do
    
     left_effect = left_temp * left_temp * left_wt - (1 - alpha) * (1 + train_to_est_ratio) 
                     * left_wt * (var_beta);
-                    
-               
+                   
+      //left_temp = left_tr_sum / left_tr - (left_sum - left_tr_sum) / (left_wt - left_tr);
+                /*left_tr_var = left_tr_sqr_sum / left_tr - 
+                    left_tr_sum  * left_tr_sum / (left_tr * left_tr);
+                left_con_var = (left_sqr_sum - left_tr_sqr_sum) / (left_wt - left_tr)  
+                    - (left_sum - left_tr_sum) * (left_sum - left_tr_sum)
+                    / ((left_wt - left_tr) * (left_wt - left_tr));        
+                left_effect = alpha * left_temp * left_temp * left_wt
+                        - (1 - alpha) * (1 + train_to_est_ratio) * left_wt 
+                    * (left_tr_var / left_tr + left_con_var / (left_wt - left_tr));*/         
 
     beta_1 = (right_n * right_yz_sum - right_z_sum * right_y_sum) / (right_n * right_yy_sum - right_y_sum * right_y_sum);
     beta_0 = (right_z_sum - beta_1 * right_y_sum) / right_n;
@@ -237,7 +245,15 @@ void CT(int n, double *y[], double *x, int nclass, int edge, double *improve, do
     right_effect = right_temp * right_temp * right_wt - (1 - alpha) * (1 + train_to_est_ratio) 
                     * right_wt * (var_beta);
                     
-
+//right_temp = right_tr_sum / right_tr - (right_sum - right_tr_sum) / (right_wt - right_tr);
+                /*right_tr_var = right_tr_sqr_sum / right_tr -
+                    right_tr_sum * right_tr_sum / (right_tr * right_tr);
+                right_con_var = (right_sqr_sum - right_tr_sqr_sum) / (right_wt - right_tr)
+                    - (right_sum - right_tr_sum) * (right_sum - right_tr_sum) 
+                    / ((right_wt - right_tr) * (right_wt - right_tr));
+                right_effect = alpha * right_temp * right_temp * right_wt
+                        - (1 - alpha) * (1 + train_to_est_ratio) * right_wt * 
+                            (right_tr_var / right_tr + right_con_var / (right_wt - right_tr));*/
                 
                 temp = left_effect + right_effect - node_effect;
                 if (temp > best) {
