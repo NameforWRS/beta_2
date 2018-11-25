5fe5d7a278aa21cbc26ba3b84c7b2634fd777669
775371 <44507768+775371@users.noreply.github.com>
2018-11-25 11:43:51 -0500

    Update init.c

--- src/init.c
+++ src/init.c
@@ -29,7 +29,7 @@ SEXP honest_estimate_rparttree(SEXP dimx, SEXP nnode, SEXP nsplit, SEXP dimc,
 
 static const R_CallMethodDef CallEntries[] = {
     {"init_ctcallback", (DL_FUNC) &init_ctcallback, 5},
-    {"causalTree", (DL_FUNC) &causalTree, 23},
+    {"causalTree", (DL_FUNC) &causalTree, 24},
     {"pred_causalTree", (DL_FUNC) &pred_causalTree, 12},
     {"estimate_causalTree", (DL_FUNC) &estimate_causalTree, 12},
     {"honest_estimate_causalTree", (DL_FUNC) &honest_estimate_causalTree, 19},
