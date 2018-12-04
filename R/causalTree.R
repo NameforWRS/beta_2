9e044814d9e4172dc0093fb1abe2bf52b65ec428
775371 <44507768+775371@users.noreply.github.com>
2018-12-04 18:03:06 -0500

    Update causalTree.R

--- R/causalTree.R
+++ R/causalTree.R
@@ -19,15 +19,14 @@ causalTree <- function(formula, data, weights, treatment, treatments, subset,
 	temp <- Call[c(1L, indx)]      
 	temp$na.action <- na.action  
 	temp[[1L]] <- quote(stats::model.frame) 
-						       
-	names(treatment) <- rownames(data)
+	m <- eval.parent(temp)					       
+	#names(treatment) <- rownames(data)
 	#Add treantment
-	names(treatments) <- rownames(data)
+	#names(treatments) <- rownames(data)
 	
-	m <- eval.parent(temp)
-	treatment <- treatment[(rownames(m))]
+	#treatment <- treatment[(rownames(m))]
 	#add
-	treatments <- treatments[(rownames(m))]
+	#treatments <- treatments[(rownames(m))]
 	
 	
 	Terms <- attr(m, "terms")
