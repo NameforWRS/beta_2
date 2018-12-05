dd95a10f43cdd855085741997fbf21edf8eb3d45
775371 <44507768+775371@users.noreply.github.com>
2018-12-04 20:10:36 -0500

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -30,12 +30,12 @@ CTinit(int n, double *y[], int maxcat, char **error,
         trsums = wtsums + maxcat;
         wtsqrsums = trsums + maxcat;
         trsqrsums = wtsqrsums + maxcat;
-            
+        Rprintf("CTinit in CT.c end\n"); 
     }
     *size = 1;
     *train_to_est_ratio = n * 1.0 / ct.NumHonest;
     return 0;
-        Rprintf("CTinit in CT.c end\n");
+       
 }
 
 void
