aaa2149fe965f7768f858c1d5af1bb010550e46c
775371 <44507768+775371@users.noreply.github.com>
2018-12-04 19:35:16 -0500

    Update myxval.c

--- src/myxval.c
+++ src/myxval.c
@@ -125,7 +125,7 @@ myxval(int n_xval, CpTable cptable_head, int *x_grp, int maxcat, char **errmsg,
         } else if (split_Rule == 2) { Rprintf("call ct.c from myxval.c\n");
             // ct:
             (*ct_eval) (k, ct.ytemp, xtree->response_est, xtree->controlMean, xtree->treatMean,
-             &(xtree->risk), ct.wtemp, ct.trtemp, ct.max_y, split_alpha, xtrain_to_est_ratio);
+             &(xtree->risk), ct.wtemp, ct.trtemp,  ct.trstemp, ct.max_y, split_alpha, xtrain_to_est_ratio);
         } else if (split_Rule == 3) {
             // fit
             (*ct_eval) (k, ct.ytemp, xtree->response_est, xtree->controlMean, xtree->treatMean, 
@@ -141,7 +141,7 @@ myxval(int n_xval, CpTable cptable_head, int *x_grp, int maxcat, char **errmsg,
         } else if (split_Rule == 6) {
             // CTD
             (*ct_eval) (k, ct.ytemp, xtree->response_est, xtree->controlMean, xtree->treatMean,
-             &(xtree->risk), ct.wtemp, ct.trtemp, ct.max_y, split_alpha, xtrain_to_est_ratio);
+             &(xtree->risk), ct.wtemp, ct.trtemp, ct.trstemp, ct.max_y, split_alpha, xtrain_to_est_ratio);
         } else if (split_Rule == 7) {
             // fitD:
             (*ct_eval) (k, ct.ytemp, xtree->response_est, xtree->controlMean, xtree->treatMean, 
